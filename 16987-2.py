824117da4c45cea918ce028be67a09025b79a424
straw <156066211+pumpsnacktv@users.noreply.github.com>
2025-04-25 08:47:23 +0000

    16987 solve ()4, 25, 25

--- 16987-2.py
+++ 16987-2.py
@@ -1,32 +1,33 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-s, w = [0]*n, [0]*n
-
 def solve(i, cnt):
     global ans
     if i == n:
         ans = max(ans, cnt)
         return
-    if s[i] <= 0:
+    if hp[i] <= 0:
         solve(i+1, cnt)
     else:
         flag = False
-        for cur in (n):
-            if i == cur or s[cur] <= 0:
+        for cur in range(n):
+            if cur == i or hp[cur] <= 0:
                 continue
             flag = True
-            s[i] -= w[cur]
-            s[cur] -= w[i]
-            solve(i+1, cnt+int(s[i]<=0)+int(s[cur]<=0))
-            s[i] += w[cur]
-            s[cur] += w[i]
+            hp[cur] -= w[i]
+            hp[i] -= w[cur]
+            solve(i+1, cnt+int(hp[cur]<1)+int(hp[i]<1))
+            hp[cur] += w[i]
+            hp[i] += w[cur]
         if flag == False:
             solve(i+1, cnt)
             
+
+n = int(input())
+hp, w = [0]*n, [0]*n
+
 for i in range(n):
-    s[i], w[i] = map(int, input().split())
+    hp[i], w[i] = map(int, input().split())
 
 ans = 0
 solve(0, 0)
--- 9251.py
+++
@@ -1,6 +0,0 @@
-def lcs()
-
-a = input()
-b = input()
-
-lcs_dp = [[-1 for _ in range(1000) for _ in range(1000)]]
