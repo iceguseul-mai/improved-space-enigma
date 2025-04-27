f3a0d16db3c187f5c6682119ecccd006fb75c5bd
straw <156066211+pumpsnacktv@users.noreply.github.com>
2025-04-27 07:57:42 +0000

    20440 resolve (25, 4, 27)

--- 20440.py
+++ 20440.py
@@ -5,31 +5,31 @@ n = int(input())
 dict = {}
 
 for i in range(n):
-    enter, leave = map(int, input().split())
-    if enter in dict:
-        dict[enter] += 1
+    start, end = map(int, input().split())
+    if start in dict.keys():
+        dict[start] += 1
     else:
-        dict[enter] = 1
-    if leave in dict:
-        dict[leave] -= 1
+        dict[start] = 1
+    if end in dict.keys():
+        dict[end] -= 1
     else:
-        dict[leave] = -1
+        dict[end] = -1
 
-mos = sorted(dict.keys())
-ans = 0
-sum_prefix = 0
-start, end = 0, 0
 flag = False
+mos = sorted(dict.keys())
+ans = -1
+start, end = 0, 0
+prefix_sum = 0
 
 for t in mos:
-    sum_prefix += dict[t]
-    if sum_prefix > ans:
-        ans = sum_prefix
+    prefix_sum += dict[t]
+    if prefix_sum > ans:
+        ans = prefix_sum
         start = t
         flag = True
-    elif sum_prefix < ans and flag:
+    elif prefix_sum < ans and flag:
         flag = False
         end = t
-
+    
 print(ans)
 print(start, end)
