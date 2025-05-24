51d4962650f1aae7cba283317b3692b82a2f5d87
straw <156066211+pumpsnacktv@users.noreply.github.com>
2025-05-24 07:25:03 +0000

    7571 resolve

--- 7571.py
+++ 7571.py
@@ -4,15 +4,16 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 r, c = [], []
 for i in range(m):
-    a,b = map(int, input().split())
+    a, b = map(int, input().split())
     r.append(a)
     c.append(b)
+
 r.sort()
 c.sort()
-mr = r[m//2]
-mc = c[m//2]
+mid_r = r[m//2]
+mid_c = c[m//2]
 ans = 0
 for i in range(m):
-    ans += abs(mr-r[i]) + abs(mc-c[i])
+    ans += abs(mid_r - r[i]) + abs(mid_c - c[i])
 
 print(ans)
